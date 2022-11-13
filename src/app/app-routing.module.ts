4f935dd1febd8700f7b0b8ee7293b98b9af097c1
DESKTOP-55M8NGQ\siva <sivagurram955@gmail.com>
2022-11-13 09:25:04 +0530

    routing path change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,32 +11,6 @@ const routes: Routes = [
 
 ];
 
-// const routes: Routes = [
-//   {
-//     path: 'reg',
-//     loadChildren: () => import('./components/registration-page/registration-page.routing.Module').then(m => m.RegistrationRoutingModule)
-//   },
-//   {
-//     path: 'login',
-//     loadChildren: () => import('./components/login-page/login-page.routing.module').then(m => m.LoginRoutingModule)
-//   },
-//   {
-//     path: 'home',
-//     loadChildren: () => import('./home-page/home-page.routing.module').then(m => m.homeRoutingModule)
-//   },
-//   {
-//     path: 'reser',
-//     loadChildren: () => import('./components/reservation-page/reservation-page.routing.module').then(m => m.ReserRoutingModule)
-//   },
-//   { path: 'reserList', component: ReservationListComponent },
-//   { path: 'reserDetails', component: ReservationDetailsComponent },
-//   {
-//     path: '',
-//     redirectTo: '',
-//     pathMatch: 'full'
-//   }
-// ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
