8c3459617e1b34805a256d5af40c9f2034d35a83
DESKTOP-55M8NGQ\siva <sivagurram955@gmail.com>
2022-11-13 09:29:46 +0530

    comments removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/DeveloperCode/dashboard/dashboard.component';
 import { HskTestComponent } from './components/DeveloperCode/hsk-test/hsk-test.component';
-// import { AuthGuardService } from './store/authguard.guard';
 
 const routes: Routes = [
   { path: 'HskTest', component: HskTestComponent },
